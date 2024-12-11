205ba4b38ab752f5a7b4d6cdac7de0abd80b148e
Stefan-101 <147538367+Stefan-101@users.noreply.github.com>
2024-12-11 15:36:12 +0200

    fix user id type and add migrations

--- Models/UserProject.cs
+++ Models/UserProject.cs
@@ -3,7 +3,7 @@ namespace Limoncello.Models
     public class UserProject 
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public virtual ApplicationUser? User { get; set; }
         public int ProjectId { get; set; }
         public virtual Project? Project { get; set; }
